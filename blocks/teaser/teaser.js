08c239368d3824e159db3f67df36dae00f77fe7e
SushEDS <mymail2ranjana@gmail.com>
2026-08-18 19:43:23 +0530

    Update teaser.js

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,14 +1,9 @@
 export default function decorate(block) {
   console.log('🔥 TEASER JS IS WORKING!', block);
-
-  // Change the heading so we can clearly see JS is running
   const heading = block.querySelector('.title h1');
-
-  if (heading) {
+   if (heading) {
     heading.textContent = '🔥 TEASER JS IS WORKING!';
   }
-
-  // Change the block look temporarily for testing
   block.style.border = '8px solid red';
   block.style.borderRadius = '30px';
   block.style.boxShadow = '0 0 30px rgba(255, 0, 0, 0.5)';
